be22d4e0bd3d988671fc8df2db0900baa6c44e5c
Dave Bascom <dave@cumulocreative.com>
2015-12-17 06:46:19 -0500

    layout change

--- client/ui/components/plantInventory.jsx
+++
@@ -1,7 +0,0 @@
-CC.PlantInventory = React.createClass ({
-  	render () {
-    	return <div>
-    		<h1>This will be a list of User's active plants</h1>
-    	</div>
-  	}
-});
---
+++ client/ui/components/possiblePlants.jsx
@@ -0,0 +1,9 @@
+CC.PossiblePlants = React.createClass ({
+  	render () {
+    	return <div>
+    		<h1>This will be a list of possible plants</h1>
+    		<h3>Users previous plants will be listed on top</h3>
+    		<h3>followed by other plants from the database. Probably similar plant types to user's previous plant tyes</h3>
+    	</div>
+  	}
+});
