5779bf1e07c3d83ff7013fa5786a8cac5eb1b0eb
Dave Bascom <dave@cumulocreative.com>
2016-01-19 08:35:32 -0500

    header count layout

--- client/ui/components/layout/header.jsx
+++ client/ui/components/layout/header.jsx
@@ -31,6 +31,42 @@ CC.Header = React.createClass ({
 	  			</a>
   			</div>
 
+  			<div className="header__count__box">
+  				<div className="header__count left">
+  					<div className="header__count__number">12</div>
+  					<div className="header__count__title">
+  						<div className="header__count__icon">
+  							<i className="fk-varieties"></i>
+  						</div>
+  						<div className="header__count__title-text">
+  							<span>varieties</span>
+  						</div>
+  					</div>
+  				</div>
+  				<div className="header__count center">
+  					<div className="header__count__number">38</div>
+  					<div className="header__count__title">
+  						<div className="header__count__icon">
+  							<i className="fk-plant"></i>
+  						</div>
+  						<div className="header__count__title-text">
+  							<span>total</span>
+  						</div>
+  					</div>
+  				</div>
+  				<div className="header__count right">
+  					<div className="header__count__number">6</div>
+  					<div className="header__count__title">
+  						<div className="header__count__icon">
+  							<i className="fk-systems"></i>
+  						</div>
+  						<div className="header__count__title-text">
+  							<span>systems</span>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+
 	  	</div>
 	  )
 	}
