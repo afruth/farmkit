b2612759760f64481e53ef6c4601a83dec9835b6
Dave Bascom <dave@cumulocreative.com>
2016-01-21 17:53:29 -0500

    almost have Redux working

--- client/ui/components/GenericComponents/SortControlButtons/SortControl.jsx
+++ client/ui/components/GenericComponents/SortControlButtons/SortControl.jsx
@@ -1,8 +1,60 @@
 CC.SortControl = React.createClass({
+	setAll(){
+		CC.store.dispatch({ type: 'all' });
+	},
+	setHydro(){
+		CC.store.dispatch({ type: 'hydro' });
+	},
+	setSoil(){
+		CC.store.dispatch({ type: 'soil' });
+	},
 	render () {
+		let state = CC.store.getState();
+		const allClass = classNames({
+			'active': state.sortAll
+		});
+		const hydroClass = classNames({
+			'active': state.sortHydro
+		});
+		const soilClass = classNames({
+			'active': state.sortSoil
+		});
 		return (
 			<div className="sort-control">
+				<div className='sort-control_all' onClick={ this.setAll }>
+					<CC.ToggleActiveDiv text="All" active={allClass} class="test" />
+				</div>
+				<div className='sort-control__hydro' onClick={ this.setHydro }>
+					<CC.ToggleActiveDiv text="Hydroponics" active={hydroClass} class="test" />
+				</div>
+				<div className='sort-control_soil' onClick={ this.setSoil }>
+					<CC.ToggleActiveDiv text="Soil" active={soilClass} class="test" />
+				</div>
+			</div>
+		)
+	}
+});
 
+
+// Renders div with passed text, and classes, combined with 'active' 
+// 		if prop.active is true
+// Props:
+//		text: 	 String
+// 		active:  boolean
+// 		classes: string of classnames
+CC.ToggleActiveDiv = React.createClass ({
+	render () {
+		console.log(this.props)
+		// const passedClasses = this.props.class;
+		const classes = classNames({
+			// passedClasses, 
+			// { 'active': this.props.active }
+			'active': this.props.active
+		});
+
+		return (
+			<div className={classes}>
+				{this.props.text}
 			</div>
 		)
 	}
--- client/ui/reduxStore.js
+++ client/ui/reduxStore.js
@@ -1,18 +1,32 @@
 
-function counter(state = 0, action) {
+CC.sortControlReducer = function sortControlReducer(state = 'all', action) {
   switch (action.type) {
-  case 'INCREMENT':
-    return state + 1
-  case 'DECREMENT':
-    return state - 1
-  default:
-    return state
+	  case 'all':
+	  	return Object.assign({}, state, { 
+	  		'sortAll': true,
+	  		'sortHydro': false,
+	  		'sortSoil': false
+	  	});
+	  case 'hydro':
+	    return Object.assign({}, state, { 
+	  		'sortAll': false,
+	  		'sortHydro': true,
+	  		'sortSoil': false
+	  	});
+	  case 'soil':
+	  	return Object.assign({}, state, { 
+	  		'sortAll': false,
+	  		'sortHydro': false,
+	  		'sortSoil': true
+	  	});
+	  default:
+	    return state;
   }
 }
 
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-CC.store = Redux.createStore(counter);
+CC.store = Redux.createStore( CC.sortControlReducer );
 
 // You can subscribe to the updates manually, or use bindings to your view layer.
 CC.store.subscribe(() =>
